123e6351648760c433343a16f2326be921a40e44
Paul Lanza <paullanza@gmail.com>
2025-05-27 11:22:41 -0400

    Can add a new tub, no photos yet

--- app/controllers/tubs_controller.rb
+++ app/controllers/tubs_controller.rb
@@ -1,9 +1,35 @@
 class TubsController < ApplicationController
+  before_action :set_tub, only: [:show]
+
   def index
     @tubs = Tub.all
   end
 
   def show
     @tub = Tub.find(params[:id])
+  end
+
+  def new
+    @tub = Tub.new
+  end
+
+  def create
+    @tub = Tub.new(tub_params)
+    @tub.user = current_user
+    if @tub.save
+      redirect_to tub_path(@tub)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_tub
+    @tub = Tub.find(params[:id])
+  end
+
+  def tub_params
+    params.require(:tub).permit(:name, :description, :price, :size, :category)
   end
 end
